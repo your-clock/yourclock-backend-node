70d3ef16e56c4f3fde09029b1b8abd04834927ee
Erney David Garcia Vergara <68959360+egarc12@users.noreply.github.com>
2021-09-26 19:30:25 -0500

    update refactor of folders

--- src/controller/api/relojControllerAPI.js
+++ src/controller/api/relojControllerAPI.js
@@ -1,8 +1,8 @@
-const relojModel = require('../../services/reloj')
+const relojService = require('../../services/reloj')
 
 exports.setData = async (req, res) => {
 	try {
-		await relojModel.emitData(req.body, req.headers.device_id)
+		await relojService.emitData(req.body, req.headers.device_id)
 		return res.status(200).send("OK")
 	} catch (error) {
 		return res.status(error.statusCode || 500).send(error.body || error.toString())
@@ -11,7 +11,7 @@ exports.setData = async (req, res) => {
 
 exports.setAlarm = async (req, res) => {
 	try {
-		const response = await relojModel.sendAlarm(req.body.time)
+		const response = await relojService.sendAlarm(req.body.time)
 		return res.status(200).json({
 			msg: "Alarma configurada exitosamente",
 			code: 308,
--- test/userService.js
+++ test/userService.js
@@ -1,28 +1,28 @@
-const userModel = require('../src/services/users')
+const userService = require('../src/services/users')
 const chai = require('chai');
 const path = require('path');
 
 chai.should();
 
-describe('Modelos de usuarios', () => {
+describe('Services de usuarios', () => {
 
-    it('model updateStateByEmail failed', async () => {
+    it('service updateStateByEmail failed', async () => {
         try {
-            await userModel.updateStateByEmail("prueba@correo.com", true);
+            await userService.updateStateByEmail("prueba@correo.com", true);
         } catch (error) {
             error.body.should.be.a('object')
         }
     });
 
-    it('model updateStateByEmail', async () => {
+    it('service updateStateByEmail', async () => {
         try {
-            await userModel.updateStateByEmail("prueba@correo.com", false);
+            await userService.updateStateByEmail("prueba@correo.com", false);
         } catch (error) {
             error.body.should.be.a('object')
         }
     });
 
-    it('model sendEmailToUser', async () => {
+    it('service sendEmailToUser', async () => {
         var mailOptions = {
             from: 'yourclocknoreply@gmail.com',
             to: "prueba@correo.com",
@@ -34,10 +34,10 @@ describe('Modelos de usuarios', () => {
             id: "123erfgbgr43rgr4efgfe3fg",
             base_url: "http://localhost:8080"
         }
-        await userModel.sendEmailToUser(mailOptions, plantilla, datos);
+        await userService.sendEmailToUser(mailOptions, plantilla, datos);
     });
 
-    it('model sendEmailToUser failed', async () => {
+    it('service sendEmailToUser failed', async () => {
         try{
             var mailOptions = {
                 from: 'yourclocknoreply@gmail.com',
@@ -50,14 +50,14 @@ describe('Modelos de usuarios', () => {
                 id: "123erfgbgr43rgr4efgfe3fg",
                 base_url: "http://localhost:8080"
             }
-            await userModel.sendEmailToUser(mailOptions, plantilla, datos);
+            await userService.sendEmailToUser(mailOptions, plantilla, datos);
         } catch (error) {
             error.body.should.be.a('object')
         }
     });
-    it('model authenticateUser failed', async () => {
+    it('service authenticateUser failed', async () => {
         try{
-            await userModel.authenticateUser(true, "123456", "123456");
+            await userService.authenticateUser(true, "123456", "123456");
         } catch (error) {
             error.body.should.be.a('object')
         }
